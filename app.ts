803660afbcf9e3526810db71d9ce0a576c815b14
MuhammadMomin095 <160387789+MuhammadMomin095@users.noreply.github.com>
2024-03-06 17:01:06 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,10 @@
+let alien_color: string ="green";
+
+if(alien_color =="green"){
+    console.log("the player just earned 5 points.");
+}
+
+alien_color = "yellow";
+if(alien_color =="green"){
+    console.log("the player just earned 5 points.");
+}
